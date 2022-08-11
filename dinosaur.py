d002a5a0de134353349ebd0406b97d6e55be077c
Monica Banda <monicabanda@Monicas-Air.lan>
2022-08-10 19:57:39 -0600

    added code for dinosaur and robot classes

--- dinosaur.py
+++ dinosaur.py
@@ -1,2 +1,12 @@
 class Dinosaur:
-    
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.health = 100
+        self.attack_power = 35
+
+
+    def attack_robot(self, robot):
+        robot.health = robot.health - (self.attack_power)
+        print(f"\n{self.name} attacked {robot.name} for {self.attack_power} damage!\n{robot.name}'s health is now {robot.health}")
+        if (robot.health <= 0):
+            print(f"\n{robot.name} has been terminated!")
--- robot.py
+++ robot.py
@@ -1,1 +1,13 @@
 class Robot:
+    def __init__(self, name, weapon):
+        self.name = name 
+        self.health = 100
+        self.weapon = weapon
+
+
+    def attack_dinosaur(self, dinosaur):
+        dinosaur.healh = dinosaur.health - (self.weapon.attack_power)
+        print(f"\n{self.name} attacked {dinosaur.name} with {self.weapon.name} for {self.weapon.attack_power} damage!\n{dinosaur.name}'s health is now {dinosaur.health}")
+        if (dinosaur.health <= 0):
+            print(f"\n{dinosaur.name} has been slayed!")
+            
